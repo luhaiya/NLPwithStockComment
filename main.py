91fc3544b9a9cccd8fa54b52707b9249aebe7caa
Black-Or-White <1021380422@qq.com>
2017-05-22 20:01:54 +0800

    Create main.py
    这里是主程序

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+Created on 2017年5月17日
+@author: luhaiya
+@id: 2016110274
+@description:
+'''
+#http://data.eastmoney.com/stockcomment/  所有股票的列表信息
+#http://guba.eastmoney.com/list,600000,5.html 某只股票股民的帖子页面
+#http://quote.eastmoney.com/sh600000.html?stype=stock 查询某只股票
+from Crawler import *
+from File import *
+import sys
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+           
+def main():
+    stocknum = str(600000)
+    page = str(1)
+    crawler = Crawler(stocknum, page)
+    datalist = crawler.getData()
+    strdata = ''
+    for data in datalist:
+        strdata += data['content']
+    txt = File(stocknum,'txt','./data/')
+    txt.inputData(strdata)
+            
+if __name__ == "__main__":
+    main()
