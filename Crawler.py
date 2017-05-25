f18f113a553713e65bfbc0695b8371d6537f92d6
Black-Or-White <1021380422@qq.com>
2017-05-25 18:34:09 +0800

    Update Crawler.py
    新增字符串去HTML标签，数据类型使用json

--- Crawler.py
+++ Crawler.py
@@ -8,6 +8,9 @@ Created on 2017��5��17��
 '''
 from selenium import webdriver
 import time
+import json
+import re  
+from HTMLParser import HTMLParser 
 # from lxml import html
 # import requests
 class Crawler:
@@ -30,6 +33,27 @@ class Crawler:
 #         return domTree
     def getNewUrl(self,url):
         self.newurl.add(url)
+    def filterHtmlTag(self, htmlStr):
+        self.htmlStr = htmlStr  
+        #先过滤CDATA  
+        re_cdata=re.compile('//<!CDATA\[[^>]*//\]>',re.I) #匹配CDATA  
+        re_script=re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I)#Script  
+        re_style=re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>',re.I)#style  
+        re_br=re.compile('<br\s*?/?>')#处理换行  
+        re_h=re.compile('</?\w+[^>]*>')#HTML标签  
+        re_comment=re.compile('<!--[^>]*-->')#HTML注释  
+        s=re_cdata.sub('',htmlStr)#去掉CDATA  
+        s=re_script.sub('',s) #去掉SCRIPT  
+        s=re_style.sub('',s)#去掉style  
+        s=re_br.sub('\n',s)#将br转换为换行  
+        blank_line=re.compile('\n+')#去掉多余的空行  
+        s = blank_line.sub('\n',s)  
+        s=re_h.sub('',s) #去掉HTML 标签  
+        s=re_comment.sub('',s)#去掉HTML注释  
+        #去掉多余的空行  
+        blank_line=re.compile('\n+')  
+        s=blank_line.sub('\n',s)  
+        return s
     def getData(self):
         comments = []
         self.crawAllHtml(self.url)
@@ -46,7 +70,7 @@ class Crawler:
             post = self.driver.find_elements_by_xpath('//*[@id="zwconbody"]/div')
             age = self.driver.find_elements_by_xpath('//*[@id="zwconttbn"]/span/span[2]')
             if len(post) and len(time) and len(age):
-                comments.append({'time':time[0].text,'content':post[0].text, 'age':age[0].text})
+                comments.append({'time':time[0].text,'content':self.filterHtmlTag(post[0].text), 'age':age[0].text})
             commentlist = self.driver.find_elements_by_xpath('//*[@id="zwlist"]/div')  
             if len(commentlist):
                 for comment in commentlist:
@@ -54,5 +78,5 @@ class Crawler:
                     post = comment.find_elements_by_xpath('./div[3]/div[1]/div[3]')
                     age = comment.find_elements_by_xpath('./div[3]/div[1]/div[1]/span[2]/span[2]')
                     if len(post) and len(time) and len(age):
-                        comments.append({'time':time[0].text,'content':post[0].text, 'age':age[0].text})
-        return comments
+                        comments.append({'time':time[0].text,'content':self.filterHtmlTag(post[0].text), 'age':age[0].text})
+        return json.dumps(comments)
